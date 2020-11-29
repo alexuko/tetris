6248d0572c9f51aae0ac52fff5da827555ca1a7a
Alejandro <alextek1885@hotmail.com>
2020-11-29 21:14:02 +0000

    rotate piece to left and right

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-import {tetrominoes} from './module/tetrominoes';
+import {
+    tetrominoes
+} from './module/tetrominoes';
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -56,21 +58,23 @@ const undrawSquare = (x, y) => {
 
 
 const colors = [14, 45, 207, 174, 66, 340, 262];
+
 const DIRECTION = {
-    right: ['right', 1],
-    left:  ['left', -1],
-    down:  ['down',  1]
+    right: ['right',  1],
+    left:  ['left' , -1],
+    down:  ['down' ,  1]
 }
 
 
 class Piece {
     constructor(tetrominoe, color) {
-        this.tetrominoe = tetrominoe; //tetrominoe from the tetrominoe array
+        this.tetrominoe = tetrominoe; //tetrominoe from the tetrominoe array with all of the positions
         this.color = color; //color from the colors array
-        this.x = 3; //position X in the canvas
+        this.x = 4; //position X in the canvas
         this.y = 0; //position Y in the canvas
-        this.activeTetrominoe = this.tetrominoe; //current view of the piece
-        this.axis = this.activeTetrominoe[1][1]; //axis point
+        this.position = 0;
+        this.activeTetrominoe = this.tetrominoe[this.position]; //tetrominow with current position
+        
     }
 
     drawPiece = () => {
@@ -79,105 +83,102 @@ class Piece {
             row.forEach((col, cIndex) => {
                 // console.log(this.activeTetrominoe[rIndex][cIndex])            
                 if (this.activeTetrominoe[rIndex][cIndex] === 1) { //if there is a "1" in the piece matrix
-                drawSquare(this.x + cIndex, this.y + rIndex, this.color); //draw a square
-            }
+                    drawSquare(this.x + cIndex, this.y + rIndex, this.color); //draw a square
+                }
             })
         });
     }
-    
+
     erasePiece = () => {
         this.activeTetrominoe.forEach((row, rIndex) => {
             row.forEach((col, cIndex) => {
                 if (this.activeTetrominoe[rIndex][cIndex] === 1) { //if there is a "1" in the piece matrix
-                undrawSquare(this.x + cIndex, this.y + rIndex); //draw a square
-                drawSquare(this.x + cIndex, this.y + rIndex); //draw a square
-            }
-        })
-    });
+                    undrawSquare(this.x + cIndex, this.y + rIndex); //draw a square
+                    drawSquare(this.x + cIndex, this.y + rIndex); //draw a square
+                }
+            })
+        });
     }
-    
-    rotate = (clockwise = true) => {
-        console.log('this.x ' + this.x)
-        console.log('this.y ' + this.y)
-        console.log('this.axis ' + this.axis)
-        
 
-        if (clockwise) {
-            console.log('clockwise')
-        }
-        
+    rotate = (clockwise = true) => {
+        this.erasePiece()        
+        // position 0 - 1 = -1 + 4 = 3 % 4 = 3
+        // position 0 + 1 = 1 % 4 = 1
+        // position 1 + 1 = 2 % 4 = 2
+        this.position =  clockwise ? (this.position + 1) % this.tetrominoe.length
+                                   : ((this.position - 1) + this.tetrominoe.length) % this.tetrominoe.length;
+        //modulo negative in JS dows
+        this.activeTetrominoe = this.tetrominoe[this.position]
+        this.drawPiece()
     }
-    moveUp = () => {
-        this.erasePiece();
-        this.y -= 1;
-        this.drawPiece();
-        
-    }
-    moveDown = () => {
-        if(!this.collision(DIRECTION.down)){
-        // if(!this.hitBottom(DIRECTION.down)){
+
+     moveDown = () => {
+        if (!this.collision(DIRECTION.down)) {
+            // if(!this.hitBottom(DIRECTION.down)){
             this.erasePiece();
             this.y += DIRECTION.down[1];
             this.drawPiece();
-            
+
         }
 
     }
-    
+
     moveRight = () => {
         //check if there is no collision and if there is not, 
         //then move tetrominoe to the right
         if (!this.collision(DIRECTION.right)) {
-        // if (!this.hitWall(DIRECTION.right)) {
+            // if (!this.hitWall(DIRECTION.right)) {
             this.erasePiece();
             this.x += DIRECTION.right[1];
             this.drawPiece();
         }
     }
-    
+
     moveLeft = () => {
         //check if there is no collision and if there is not, 
         //then move tetrominoe to the left 
         if (!this.collision(DIRECTION.left)) {
-        // if (!this.hitWall(DIRECTION.left)) {
+            // if (!this.hitWall(DIRECTION.left)) {
             this.erasePiece();
             this.x -= -DIRECTION.left[1];
             this.drawPiece();
         }
     }
 
-    collision = (dir) => {        
+    collision = (dir) => {
         for (let r = 0; r < this.activeTetrominoe.length; r++) { //loop through all of the rows
             for (let c = 0; c < this.activeTetrominoe[r].length; c++) { //for each row loop through all of the columns
                 if (!this.activeTetrominoe[r][c]) continue; // skip zeros in the tetrominoe matrix
                 // get new X coordinates for each square of the tetrominoe
                 // e.g 3 + 0 + 1 (moveRight) = 4    
                 // e.g 3 + 0 - 1 (moveLeft) =  2    
-                let new_x = this.x + c + dir[1]; 
+                let new_x = this.x + c + dir[1];
                 let new_y = this.y + r + dir[1];
-                if(dir[0] === 'down'){// if Direction is going DOWN then check "Y" limits
-                    if(new_y >= ROW) return true;  
-                    if(new_y <= ROW) continue; // current square from tetrominoe is ok. So check NEXT
-                }else{// if Direction is either LEFT or RIGHT then check "X" limits
-                    if(new_x < 0 || new_x >= COL) return true; 
+                if (dir[0] === 'down') { 
+                    // if Direction is going DOWN then check "Y" limits
+                    if (new_y >= ROW) return true; //reached the limit, is has collided
+                    if (new_y <= ROW) continue; // current square from tetrominoe is ok. So check NEXT
+                } else { 
+                    // if Direction is either LEFT or RIGHT then check "X" limits
+                    if (new_x < 0 || new_x >= COL) return true;
                 }
 
             }
         }
         return false; // if no collision then if ok to move tetrominoe
-    }    
-    
+    }
+
 }
 
 
-let piece = new Piece(tetrominoes[3][0], colors[0])
+let piece = new Piece(tetrominoes[0], colors[0])
 
 let start = Date.now()
 const update = () => {
     let now = Date.now()
     let timeCounter = now - start;
     const sec = 1000 / level;
-    
+
     if (timeCounter > sec) {
         piece.moveDown()
         start = Date.now();
@@ -187,10 +188,13 @@ const update = () => {
 }
 
 const keyControl = (e) => {
+    // console.log(e)
     if (e.type === "keydown") {
+        if (e.key === "KeyZ" || e.keyCode === 90) {
+            piece.rotate(false);
+        }
         if (e.key === "ArrowUp" || e.keyCode === 38) {
-            // piece.rotate()
-            piece.moveUp()
+            piece.rotate()
         }
         if (e.key === "ArrowRight" || e.keyCode === 39) {
             piece.moveRight();
